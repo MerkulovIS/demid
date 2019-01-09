f1b02f9ea08d1479dd85096466e6c5ecd8c361a5
MerkulovIS <imerkulov@me.com>
2019-01-09 18:11:38 +0300

    Initial Commit

---
+++ demid/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  demid
+//
+//  Created by Ilya Merkulov on 09/01/2019.
+//  Copyright © 2019 Ilya Merkulov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
